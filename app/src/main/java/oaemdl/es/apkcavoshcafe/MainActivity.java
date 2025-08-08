1147edc0344ced0e5961f7b2937aa21c39128abc
oaemdl <oaemdl@gmail.com>
2025-08-08 13:44:06 -0500

    android

---
+++ app/src/main/java/oaemdl/es/apkcavoshcafe/MainActivity.java
@@ -0,0 +1,41 @@
+package oaemdl.es.apkcavoshcafe;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import oaemdl.es.apkcavoshcafe.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        navController.addOnDestinationChangedListener( (navController1, navDestination, bundle) ->  {
+           int id =  navDestination.getId();
+           if ( id == R.id.navigation_splash ||
+                id == R.id.navigation_login ||
+                id == R.id.navigation_registrar )
+               navView.setVisibility( View.INVISIBLE );
+        });
+    }
+
+}
---
+++ app/src/main/java/oaemdl/es/apkcavoshcafe/ui/Login.java
@@ -0,0 +1,53 @@
+package oaemdl.es.apkcavoshcafe.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import oaemdl.es.apkcavoshcafe.R;
+import oaemdl.es.apkcavoshcafe.databinding.FragmentLoginBinding;
+
+public class Login extends Fragment {
+    FragmentLoginBinding binding;
+    View view;
+    Context context;
+    NavController navController;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return view = binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController( view );
+
+        binding.btnLogin.setOnClickListener( v -> btnLogin_Click() );
+        binding.btnRegister.setOnClickListener( v -> navController.navigate( R.id.navigation_registrar ) );
+        binding.tvRegisterNow.setOnClickListener( v -> navController.navigate( R.id.navigation_registrar ) );
+        binding.tvForgotPasswordd.setOnClickListener( v -> navController.navigate( R.id.navigation_passwordd ) );
+    }
+
+    private void btnLogin_Click() {
+
+    }
+
+}
